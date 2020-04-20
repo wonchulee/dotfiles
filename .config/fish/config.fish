b0f713c7fe607d0a2e8d88056f0e316ed90c3451
Wonchul Lee <w.lee@lge.com>
2020-04-20 10:37:55 +0900

    Add various config for fish and tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,11 @@ alias vim='vimx'
 
 set -gx NPM_PACKAGES_BIN $HOME/.npm-packages/bin
 set -gx PATH $HOME/.local/bin $NPM_PACKAGES_BIN $PATH
+set -gx MANPATH $MANPATH $NPM_PACKAGES/share/man
+
+set -gx LD_LIBRARY_PATH ~/.local/lib64 ~/.local/lib $LD_LIBRARY_PATH
+set -gx PKG_CONFIG_PATH ~/.local/lib64/pkgconfig ~/.local/lib/pkgconfig $PKG_CONFIG_PATH
 
 set -gx EDITOR vim
+
+set -gx GTK_IM_MODULE ibus
