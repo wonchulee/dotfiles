d897fcb96aeb39731be16badb0c283fe0ca24533
Wonchul Lee <w.lee@lge.com>
2020-05-25 16:33:14 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 alias vi='vimx'
 alias vim='vimx'
 
+alias cat='bat'
+alias grep='rg'
+
 set -gx NPM_PACKAGES_BIN $HOME/.npm-packages/bin
 set -gx PATH $HOME/.local/bin $NPM_PACKAGES_BIN $PATH
 set -gx MANPATH $MANPATH $NPM_PACKAGES/share/man
