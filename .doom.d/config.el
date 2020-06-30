18e6f6701135acd0ddadd6b423de5e380d650c82
Wonchul Lee <wonchul.dev@gmail.com>
2020-06-30 22:54:42 +0900

    emacs: Update config
    It changes theme to doom-palenight and add prettier-js minor mode

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -25,7 +25,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-palenight)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -80,6 +80,14 @@
 )
 (add-hook 'web-mode-hook  'web-mode-init-hook)
 
+(require' prettier-js)
+(after! prettier-js
+         (map! :leader
+             :prefix "m"
+             :desc "prettier-js" "p" #'prettier-js))
+(add-hook 'js2-mode-hook #'(lambda ()
+                            (enable-minor-mode
+                             '("\\.jsx?\\'" . prettier-js-mode))))
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
